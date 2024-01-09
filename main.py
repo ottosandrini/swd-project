b071cc29b990cb1f29e9b82ed65c09a99f0846df
Mint <RnTPVGas@users.noreply.github.com>
2024-01-09 17:45:58 +0100

    made main.py look acceptable

--- main.py
+++ main.py
@@ -4,40 +4,9 @@ import streamlit as st
 from queries import find_devices
 from devices import Device
 
+st.set_page_config(page_title="Geräte Verwaltung")
+
 st.write("# Gerätemanagement")
-st.write("## Was möchten sie gerne tun:")
-
-#multiple buttons going to different pages:
+st.write("## <- Wählen sie hier eine Seite")
 
 
-"""
-# Eine Auswahlbox mit Datenbankabfrage, das Ergebnis wird in current_device gespeichert
-devices_in_db = find_devices()
-
-if devices_in_db:
-    current_device_name = st.selectbox(
-        'Gerät auswählen',
-        options = devices_in_db, key="sbDevice")
-
-    if current_device_name in devices_in_db:
-        loaded_device = Device.load_data_by_device_name(current_device_name)
-        st.write(f"Loaded Device: {loaded_device}")
-
-
-    with st.form("Device"):
-        st.write(loaded_device.device_name)
-
-        #checkbox_val = st.checkbox("Is active?", value=loaded_device.is_active)
-        #loaded_device.is_active = checkbox_val
-
-        text_input_val = st.text_input("Geräte-Verantwortlicher", value=loaded_device.responsible_person)
-        loaded_device.responsible_person = text_input_val
-
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            loaded_device.store_data()
-            st.write("Data stored.")
-            st.rerun()
-"""
-#test
